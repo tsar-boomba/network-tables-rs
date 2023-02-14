3a1fe9ea66a94e4f282b858c7e6fa0885a6dbef7
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2023-02-14 07:36:36 -0500

    cargo fmt for release

--- tools/release.sh
+++ tools/release.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cargo fmt --all
-git add .
-git commit -m "cargo fmt for release"
+git commit -a -m "cargo fmt for release"
 cargo release --workspace --allow-branch main --tag-name 'v{{version}}' --all-features -v -x $1
